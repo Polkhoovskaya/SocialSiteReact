a82a59c6a0aa64c20859522b7d196e8d86fd5800
Palkhouskaya <vpalkhouskaya@scnsoft.com>
2020-10-01 16:40:17 +0300

    Updates

---
+++ precache-manifest.b34cec1e8fd8e6405a1ae17bcd339ada.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1ec56e8aac204824f8942fbd78b6ce",
+    "url": "/SocialSiteReact/index.html"
+  },
+  {
+    "revision": "2e28b03652f825d296ad",
+    "url": "/SocialSiteReact/static/css/main.3b0016f0.chunk.css"
+  },
+  {
+    "revision": "e9f2d1ab2c74b826e268",
+    "url": "/SocialSiteReact/static/js/2.447cf27d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialSiteReact/static/js/2.447cf27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e28b03652f825d296ad",
+    "url": "/SocialSiteReact/static/js/main.ba5c33b7.chunk.js"
+  },
+  {
+    "revision": "c1d358d3b147c319ebc0",
+    "url": "/SocialSiteReact/static/js/runtime-main.48d64a53.js"
+  },
+  {
+    "revision": "e9aff503f2e5e67a276f301c15d19b1c",
+    "url": "/SocialSiteReact/static/media/avatar.e9aff503.jfif"
+  },
+  {
+    "revision": "e2f0d91998199c837356fd3815ce2cea",
+    "url": "/SocialSiteReact/static/media/preloader.e2f0d919.svg"
+  }
+]);
